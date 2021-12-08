94167dbec34a3e0e8cd20d7a3aff353ac6e2b88b
sikka-tanu <sikka.tanu@gmail.com>
2021-12-08 17:53:35 +0530

    Update index file

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import { User } from "./models/User";
 
 const user = new User({ name: 'myname', age: 20});
-console.log(user.get('name'));
-console.log(user.get('age'));
 
 user.set({'name': 'newname'});
 
-console.log(user.get('name'));
-console.log(user.get('age'));
+user.on('change', () => {
+    console.log('Change triggered')
+});
+
+user.trigger('change');
